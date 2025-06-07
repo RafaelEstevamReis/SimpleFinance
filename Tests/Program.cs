a695c62dc295cacf0ab82fbde86561b74212c76a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-06-07 20:41:21 -0300

    Add Sample functions

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Simple.Finance TEST");
 
 var mngr = new Simple.Finance.Manager("data.db");
 mngr.Initialize(createBackup: true, backupName: $"data_{DateTime.Now:yyyyMMddHH}.db");
+
+Tests.SampleFunctions.Run(mngr);
+Console.WriteLine("\nEND");
---
+++ Tests/SampleFunctions.cs
@@ -0,0 +1,120 @@
+﻿namespace Tests;
+
+using System;
+using System.Linq;
+
+public class SampleFunctions
+{
+    static Random Rnd = new();
+    public static void Run(Simple.Finance.Manager mgr)
+    {
+        Console.WriteLine(">> Sample Functions\n");
+
+        /* Wallets */
+        var wallets = mgr.GetWallets().ToArray();
+        if (wallets.Length == 0)
+        {
+            mgr.CreateUpdateWallet(new Simple.Finance.Tables.Wallet()
+            {
+                Id = 0,
+                Name = "Default Wallet",
+                Description = "",
+                IsDeleted = false,
+            });
+            wallets = mgr.GetWallets().ToArray();
+        }
+
+        /* Categories */
+        var cats = mgr.GetCategories().ToArray();
+        if (cats.Length == 0)
+        {
+            mgr.CreateUpdateCategory(new Simple.Finance.Tables.Category()
+            {
+                Id = 0,
+                Name = "My House Expenses",
+                Description = "",
+                IsExpense = true,
+                IsDeleted = false,
+            });
+            mgr.CreateUpdateCategory(new Simple.Finance.Tables.Category()
+            {
+                Id = 0,
+                Name = "My Food Expenses",
+                Description = "",
+                IsExpense = true,
+                IsDeleted = false,
+            });
+            mgr.CreateUpdateCategory(new Simple.Finance.Tables.Category()
+            {
+                Id = 0,
+                Name = "Income",
+                Description = "",
+                IsExpense = false,
+                IsDeleted = false,
+            });
+            cats = mgr.GetCategories().ToArray();
+        }
+        Console.WriteLine($"{cats.Length} Categories");
+        Console.WriteLine(string.Join("\n", cats.Select(x => $" * {x.Id}: [{(x.IsExpense ? "EXP" : "INC")}] {x.Name}")));
+
+        /* Transactions */
+        var txVal = Math.Round((decimal)(Rnd.NextDouble() * 100), 2);
+        mgr.CreateUpdateTransaction(new Simple.Finance.Tables.Transac
+        {
+            Id = 0,
+            CategoryId = cats.Where(o => o.IsExpense).First().Id,
+            WalletId = wallets[0].Id,
+            Description = $"My Expense Of {DateTime.Now}",
+            DueValue = txVal,
+            PaidValue = txVal,
+            Status = Simple.Finance.Tables.Transac.PaymentStatus.Paid,
+            DueDate = DateTime.Now,
+            PaymentDate = DateTime.Now,
+        });
+        if (Rnd.NextDouble() < 0.1)
+        {
+            txVal = Math.Round((decimal)(250 + Rnd.NextDouble() * 500), 0);
+            mgr.CreateUpdateTransaction(new Simple.Finance.Tables.Transac
+            {
+                Id = 0,
+                CategoryId = cats.Where(o => !o.IsExpense).First().Id,
+                WalletId = wallets[0].Id,
+                Description = $"Income of {DateTime.Now}",
+                DueValue = txVal,
+                PaidValue = txVal,
+                Status = Simple.Finance.Tables.Transac.PaymentStatus.Paid,
+                DueDate = DateTime.Now,
+                PaymentDate = DateTime.Now,
+            });
+        }
+
+        Console.WriteLine($"{wallets.Length} Wallets");
+        Console.WriteLine(string.Join("\n", wallets.Select(x => $" * {x.Id}: {x.Name} - Bal: {mgr.GetWalletBalance(x.Id):N2}")));
+
+        var recent = mgr.GetTransactions(Simple.Finance.Manager.SearchTransactionsDate.PaymentDate, DateTime.Now.AddYears(-1), DateTime.Now.AddDays(1))
+                        .OrderBy(o => o.PaymentDate)
+                        .TakeLast(10)
+                        .ToArray();
+
+        decimal total = 0;
+        Console.WriteLine("Recent:");
+        Console.WriteLine(
+                $"* {"Id",-5} " +
+                $"{"Description",-40} " +
+                $"{"Date",-25} " +
+                $"{"Value",-8:F2} " +
+                $"{"Total",-10:F2}");
+        foreach (var tx in recent)
+        {
+            total += tx.PaidValue;
+            Console.WriteLine(
+                $"* {tx.Id,-5} " +
+                $"{tx.Description,-40} " +
+                $"{tx.PaymentDate,-25} " +
+                $"{tx.PaidValue,-10:F2} " +
+                $"{total,-10:F2}");
+        }
+
+
+    }
+}
