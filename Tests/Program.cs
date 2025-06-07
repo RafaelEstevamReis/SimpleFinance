206bb0c144350dcaed2357094c6f6e8984349ad6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-06-07 12:52:37 -0300

    Add test program

---
+++ Tests/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Simple.Finance TEST");
+
+var mngr = new Simple.Finance.Manager("data.db");
+mngr.Initialize(createBackup: true, backupName: $"data_{DateTime.Now:yyyyMMddHH}.db");
