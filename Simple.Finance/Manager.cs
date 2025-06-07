c9fd9e64623343fa29bb9e7ec81bff19a34556df
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-06-07 12:51:52 -0300

    Update Manager.cs

--- Simple.Finance/Manager.cs
+++ Simple.Finance/Manager.cs
@@ -3,27 +3,35 @@
 using Simple.Sqlite;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 
 public class Manager
 {
     private readonly ConnectionFactory db;
+    private readonly string dbFile;
 
     public Manager(string dbFile)
     {
         db = ConnectionFactory.FromFile(dbFile);
+        this.dbFile = dbFile;
     }
 
+    /// <summary>
+    /// Initializes database and backups it
+    /// </summary>
+    /// <param name="createBackup">If a backup should be created</param>
+    /// <param name="backupName">Backup file name. It will be compressed and '.gz' appended</param>
     public void Initialize(bool createBackup = false, string? backupName = null)
     {
-        using var cnn = db.GetConnection();
-
         if (createBackup)
         {
-            if(backupName is null) throw new ArgumentNullException(nameof(backupName), "backupName cannot be null");
-            cnn.CreateBackup(backupName);
+            if (backupName is null) throw new ArgumentNullException(nameof(backupName), "backupName cannot be null");
+            compress(dbFile, backupName);
         }
 
+        using var cnn = db.GetConnection();
         cnn.CreateTables()
            .Add<Tables.ChangeLog>()
            .Add<Tables.Category>()
@@ -32,6 +40,16 @@ public class Manager
            .Add<Tables.Transac>()
            .Commit();
     }
+    static void compress(string dbFile, string destFile)
+    {
+        var fiOrg = new FileInfo(dbFile);
+        var fiDest = new FileInfo(destFile);
+        using FileStream originalFileStream = File.OpenRead(fiOrg.FullName);
+        using FileStream compressedFileStream = File.Create(fiDest.FullName + ".gz");
+        using GZipStream compressionStream = new(compressedFileStream, CompressionMode.Compress);
+        originalFileStream.CopyTo(compressionStream);
+    }
+
 
     #region Wallets
 
@@ -152,7 +170,7 @@ public class Manager
             _ => throw new InvalidOperationException("Invalid date type"),
         };
 
-        if(dateType == SearchTransactionsDate.PaymentDate)
+        if (dateType == SearchTransactionsDate.PaymentDate)
         {
             add = "AND PaymentStatus = @statusPaid";
         }
@@ -160,7 +178,9 @@ public class Manager
         using var cnn = db.GetConnection();
         return cnn.Query<Tables.Transac>($"SELECT * FROM {nameof(Tables.Transac)} WHERE {kindColumn} = @id AND ({dateColumn} BETWEEN @start AND @end ) {add} ", new
         {
-            id, start, end,
+            id,
+            start,
+            end,
             statusPaid = Tables.Transac.PaymentStatus.Paid,
         });
     }
