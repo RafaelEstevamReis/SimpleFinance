02f0b9af6447e4c89493d337e433e6811449414e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-11-07 23:47:04 -0300

    Adiciona CNAB400 RET Pagamento

---
+++ Simple.Finance/Importers/CNAB/CNAB400/CobrancaDetalheModelo.cs
@@ -0,0 +1,84 @@
+﻿namespace Simple.Finance.Importers.CNAB.CNAB400;
+
+using System;
+using TextSerializer;
+using TextSerializer.Attributes;
+
+[RegistrySize(400)]
+public record CobrancaDetalheModelo
+{
+    // Usado de Base: SICRED
+
+    [Index(1), Type(DataType.N), Length(1)]
+    public string IdDetalhe { get; set; } = string.Empty; // 1
+    [Index(2), Type(DataType.C), Length(1)]
+    public string TipoCarteira { get; set; } = string.Empty;
+    [Index(3), Type(DataType.C), Length(11)]
+    public string Filler1 { get; set; } = string.Empty;
+
+    [Index(4), Type(DataType.C), Length(1)]
+    public string TipoCobranca { get; set; } = string.Empty;
+    [Index(5), Type(DataType.C), Length(5)]
+    public string CodigoPagadorBanco { get; set; } = string.Empty;
+    [Index(6), Type(DataType.C), Length(5)]
+    public string CodigoPagadorEmpresa { get; set; } = string.Empty;
+    [Index(7), Type(DataType.N), Length(1)]
+    public string BoletoDDA { get; set; } = string.Empty;
+    [Index(8), Type(DataType.N), Length(22)]
+    public string Filler2 { get; set; } = string.Empty;
+    [Index(9), Type(DataType.N), Length(15)]
+    public string NossoNumero { get; set; } = string.Empty;
+
+    [Index(10), Type(DataType.C), Length(46)]
+    public string Filler3 { get; set; } = string.Empty;
+    [Index(11), Type(DataType.C), Length(2)]
+    public string Ocorrencia { get; set; } = string.Empty;
+    [Index(12), Type(DataType.N), Length(6)]
+    public string DataOcorrencia { get; set; } = string.Empty;
+    [Index(13), Type(DataType.C), Length(10)]
+    public string SeuNumero { get; set; } = string.Empty;
+    [Index(14), Type(DataType.C), Length(20)]
+    public string Filler4 { get; set; } = string.Empty;
+    [Index(15), Type(DataType.N), Length(6)]
+    public string DataVencimento { get; set; } = string.Empty;
+    [Index(16), Type(DataType.N), Length(13, 2)]
+    public decimal ValorTitulo { get; set; }
+    [Index(17), Type(DataType.C), Length(9)]
+    public string Filler5 { get; set; } = string.Empty;
+    [Index(18), Type(DataType.C), Length(1)]
+    public string Especie { get; set; } = string.Empty;
+    [Index(19), Type(DataType.N), Length(13, 2)]
+    public decimal DespesasCobranca { get; set; }
+    [Index(20), Type(DataType.N), Length(13, 2)]
+    public decimal DespesasProcesso { get; set; }
+
+    [Index(21), Type(DataType.C), Length(26)]
+    public string Filler6 { get; set; } = string.Empty;
+
+    [Index(22), Type(DataType.N), Length(13, 2)]
+    public decimal AbatimentoConcedido { get; set; }
+    [Index(23), Type(DataType.N), Length(13, 2)]
+    public decimal DescontoConcedido { get; set; }
+    [Index(24), Type(DataType.N), Length(13, 2)]
+    public decimal ValorPago { get; set; }
+    [Index(25), Type(DataType.N), Length(13, 2)]
+    public decimal JurosMora { get; set; }
+    [Index(26), Type(DataType.N), Length(13, 2)]
+    public decimal Multa { get; set; }
+    [Index(27), Type(DataType.C), Length(2)]
+    public string Filler7 { get; set; } = string.Empty;
+    [Index(28), Type(DataType.C), Length(1)]
+    public string ExOcorrencia19 { get; set; } = string.Empty;
+    [Index(29), Type(DataType.C), Length(23)]
+    public string Filler8 { get; set; } = string.Empty;
+    [Index(30), Type(DataType.C), Length(10)]
+    public string MotivoOcorrencia { get; set; } = string.Empty;
+    [Index(31), Type(DataType.N), Length(8)]
+    public string DataPrevisaoLancamento { get; set; } = string.Empty;
+    [Index(32), Type(DataType.C), Length(58)]
+    public string Filler9 { get; set; } = string.Empty;
+    [Index(33), Type(DataType.N), Length(6)]
+    public string SeqRegistro { get; set; } = string.Empty;
+
+
+}
---
+++ Simple.Finance/Importers/CNAB/CNAB400/CobrancaHeaderModel.cs
@@ -0,0 +1,44 @@
+﻿namespace Simple.Finance.Importers.CNAB.CNAB400;
+
+using System;
+using TextSerializer;
+using TextSerializer.Attributes;
+
+[RegistrySize(400)]
+public record CobrancaHeaderModel
+{
+    // Usado de Base: SICRED
+
+    [Index(1), Type(DataType.N), Length(1)]
+    public string IdHeader { get; set; } = string.Empty; // 0
+    [Index(2), Type(DataType.N), Length(1)]
+    public string IdRemessa { get; set; } = string.Empty; // 2
+    [Index(3), Type(DataType.C), Length(7)]
+    public string LiteralRetorno { get; set; } = string.Empty; // RETORNO
+    [Index(4), Type(DataType.N), Length(2)]
+    public string CodServicoCobranca { get; set; } = string.Empty; // 01
+    [Index(5), Type(DataType.C), Length(15)]
+    public string LiteralCobranca { get; set; } = string.Empty; // COBRANCA
+    [Index(6), Type(DataType.C), Length(5)]
+    public string CodBeneficiario { get; set; } = string.Empty;
+    [Index(7), Type(DataType.C), Length(14)]
+    public string DocBeneficiario { get; set; } = string.Empty;
+    [Index(8), Type(DataType.C), Length(31)]
+    public string Filler1 { get; set; } = string.Empty;
+    [Index(9), Type(DataType.N), Length(3)]
+    public string NumeroBanco { get; set; } = string.Empty;
+    [Index(10), Type(DataType.C), Length(15)]
+    public string NomeBanco { get; set; } = string.Empty;
+    [Index(11), Type(DataType.N), Length(8)]
+    public string DataArquivo { get; set; } = string.Empty; // yyyyMMdd
+    [Index(13), Type(DataType.C), Length(8)]
+    public string Filler2 { get; set; } = string.Empty;
+    [Index(14), Type(DataType.C), Length(7)]
+    public string NumeroRetorno { get; set; } = string.Empty;
+    [Index(15), Type(DataType.C), Length(272)]
+    public string Filler3 { get; set; } = string.Empty;
+    [Index(16), Type(DataType.N), Length(5)]
+    public string Versao { get; set; } = string.Empty;
+    [Index(17), Type(DataType.N), Length(6)]
+    public string SeqRegistro { get; set; } = string.Empty;
+}
