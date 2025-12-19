49d2df3aaed41cfea980d445eba5996e079d8026
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-12-19 15:49:21 -0300

    Create DateHelpers.cs

---
+++ Simple.Finance/Helpers/DateHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple.Finance.Helpers;
+
+using System;
+
+public static class DateHelpers
+{
+    public static DateTime StartOfMonth(this DateTime dt)
+    {
+        return new DateTime(dt.Year, dt.Month, 1, 0, 0, 0, dt.Kind);
+    }
+    public static DateTime EndOfMonth(this DateTime dt)
+    {
+        var start = StartOfMonth(dt);
+        return start.AddMonths(1).AddMilliseconds(-1);
+    }
+}
