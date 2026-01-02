304ea8960f7efa6af224034100c45a5aa472e1e8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-01-02 13:46:29 -0300

    dlgAddBulkTransactions Progress
    Commited after visual studio bugged the columns for the second time

---
+++ DemoProject/Dialogs/dlgAddBulkTransactions.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace DemoProject.Dialogs
+{
+    partial class dlgAddBulkTransactions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            grdData = new System.Windows.Forms.DataGridView();
+            label1 = new System.Windows.Forms.Label();
+            cboWallet = new System.Windows.Forms.ComboBox();
+            btnSave = new System.Windows.Forms.Button();
+            clnDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            clnType = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            clnCategory = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            clnDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            clnValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            clnPaid = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)grdData).BeginInit();
+            SuspendLayout();
+            // 
+            // grdData
+            // 
+            grdData.AllowUserToResizeColumns = false;
+            grdData.AllowUserToResizeRows = false;
+            grdData.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grdData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { clnDate, clnType, clnCategory, clnDescription, clnValue, clnPaid });
+            grdData.Location = new System.Drawing.Point(0, 37);
+            grdData.Name = "grdData";
+            grdData.RowHeadersWidth = 21;
+            grdData.Size = new System.Drawing.Size(800, 416);
+            grdData.TabIndex = 0;
+            grdData.CellValidating += grdData_CellValidating;
+            grdData.CellValueChanged += grdData_CellValueChanged;
+            grdData.DataError += grdData_DataError;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(7, 9);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(43, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Wallet:";
+            // 
+            // cboWallet
+            // 
+            cboWallet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            cboWallet.FormattingEnabled = true;
+            cboWallet.Location = new System.Drawing.Point(56, 6);
+            cboWallet.Name = "cboWallet";
+            cboWallet.Size = new System.Drawing.Size(137, 23);
+            cboWallet.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new System.Drawing.Point(662, 459);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new System.Drawing.Size(131, 23);
+            btnSave.TabIndex = 9;
+            btnSave.Text = "Create Transactions";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // clnDate
+            // 
+            dataGridViewCellStyle1.Format = "d";
+            clnDate.DefaultCellStyle = dataGridViewCellStyle1;
+            clnDate.HeaderText = "Date";
+            clnDate.Name = "clnDate";
+            clnDate.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            clnDate.Width = 80;
+            // 
+            // clnType
+            // 
+            clnType.HeaderText = "Type";
+            clnType.Name = "clnType";
+            clnType.Width = 90;
+            // 
+            // clnCategory
+            // 
+            clnCategory.HeaderText = "Category";
+            clnCategory.Name = "clnCategory";
+            clnCategory.Width = 150;
+            // 
+            // clnDescription
+            // 
+            clnDescription.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            clnDescription.HeaderText = "Description";
+            clnDescription.Name = "clnDescription";
+            clnDescription.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // clnValue
+            // 
+            dataGridViewCellStyle2.Format = "N2";
+            clnValue.DefaultCellStyle = dataGridViewCellStyle2;
+            clnValue.HeaderText = "Value";
+            clnValue.Name = "clnValue";
+            clnValue.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // clnPaid
+            // 
+            clnPaid.HeaderText = "Paid";
+            clnPaid.Name = "clnPaid";
+            clnPaid.Width = 50;
+            // 
+            // dlgAddBulkTransactions
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 490);
+            Controls.Add(btnSave);
+            Controls.Add(cboWallet);
+            Controls.Add(label1);
+            Controls.Add(grdData);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            Name = "dlgAddBulkTransactions";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Add Bulk Transactions";
+            Load += dlgAddBulkTransactions_Load;
+            ((System.ComponentModel.ISupportInitialize)grdData).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboWallet;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnDate;
+        private System.Windows.Forms.DataGridViewComboBoxColumn clnType;
+        private System.Windows.Forms.DataGridViewComboBoxColumn clnCategory;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnValue;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn clnPaid;
+    }
+}
---
+++ DemoProject/Dialogs/dlgAddBulkTransactions.cs
@@ -0,0 +1,145 @@
+﻿using Simple.Finance;
+using Simple.Finance.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DemoProject.Dialogs
+{
+    public partial class dlgAddBulkTransactions : Form
+    {
+        private Category[] categories = [];
+        private Wallet[] wallets;
+        private Transac[] initTransactions = [];
+
+        public dlgAddBulkTransactions()
+        {
+            InitializeComponent();
+        }
+        private void dlgAddBulkTransactions_Load(object sender, EventArgs e)
+        {
+            cboWallet.DataSource = wallets;
+            cboWallet.ValueMember = "Id";
+            cboWallet.DisplayMember = "Name";
+
+            grdData.AutoGenerateColumns = false;
+            List<Types> types = [
+                new Types{ IsExpense = true, Name ="Expense" },
+                new Types{ IsExpense = false, Name= "Income" },
+            ];
+            clnType.DataSource = types;
+            clnType.ValueMember = "IsExpense";
+            clnType.DisplayMember = "Name";
+
+            clnCategory.DataSource = new Category[0];
+            clnCategory.ValueMember = "Id";
+            clnCategory.DisplayMember = "Name";
+
+            foreach (var tx in initTransactions)
+            {
+                bool isExpense = tx.DueValue < 0;
+                var ix = grdData.Rows.Add(tx.EfectiveDate, isExpense, null, tx.Description, Math.Abs(tx.EfectiveValue), tx.Status == Transac.PaymentStatus.Paid);
+                updateCategoryCell(ix, isExpense);
+                grdData.Rows[ix].Tag = tx;
+            }
+        }
+
+        private void grdData_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex < 0) return;
+            if (e.RowIndex < 0) return;
+
+            if (e.ColumnIndex == clnType.Index)
+            {
+                var t = grdData[e.ColumnIndex, e.RowIndex].Value;
+                if (t is bool bExpense)
+                {
+                    updateCategoryCell(e.RowIndex, bExpense);
+                }
+
+            }
+
+        }
+
+        private void updateCategoryCell(int rowIndex, bool isExpense)
+        {
+            var cboCell = grdData[clnCategory.Index, rowIndex] as DataGridViewComboBoxCell;
+            if (cboCell == null) return;
+
+            var availableCategories = categories.Where(o => o.IsExpense == isExpense).ToArray();
+            cboCell.DataSource = availableCategories;
+            cboCell.ValueMember = "Id";
+            cboCell.DisplayMember = "Name";
+            cboCell.Value = availableCategories.FirstOrDefault()?.Id;
+        }
+
+        private void grdData_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            if (e.ColumnIndex == clnDate.Index)
+            {
+                if (DateTime.TryParse(e.FormattedValue?.ToString(), out DateTime dt))
+                {
+                    grdData[e.ColumnIndex, e.RowIndex].Value = dt.Date;
+                }
+                else
+                {
+                    grdData[e.ColumnIndex, e.RowIndex].Value = DateTime.Now.Date;
+                }
+            }
+            if (e.ColumnIndex == clnValue.Index)
+            {
+                if (decimal.TryParse(e.FormattedValue?.ToString(), out decimal val))
+                {
+                    grdData[e.ColumnIndex, e.RowIndex].Value = Math.Round(Math.Abs(val), 2);
+                }
+                else
+                {
+                    grdData[e.ColumnIndex, e.RowIndex].Value = 0M;
+                }
+            }
+
+        }
+
+        private void grdData_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            //if (e.ColumnIndex == 2)
+            //{
+            //    e.Cancel = true;
+            //    return;
+            //}
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            List<Transac> lst = [];
+            foreach (DataGridViewRow row in grdData.Rows)
+            {
+                DateTime date = (DateTime)row.Cells[clnDate.Index].Value;
+                Types type = (Types)row.Cells[clnType.Index].Value;
+                long category = (long)row.Cells[clnCategory.Index].Value;
+                string description = row.Cells[clnDescription.Index].Value?.ToString() ?? "";
+                decimal value = (decimal)row.Cells[clnDescription.Index].Value;
+                bool paid = (bool)row.Cells[clnPaid.Index].Value;
+
+
+            }
+        }
+
+
+        public static DialogResult ShowDialog(Manager manager, Transac[] trs)
+        {
+            using var frm = new dlgAddBulkTransactions();
+            frm.categories = manager.GetCategories().ToArray();
+            frm.wallets = manager.GetWallets().ToArray();
+            frm.initTransactions = trs;
+            return frm.ShowDialog();
+        }
+
+        public record Types
+        {
+            public bool IsExpense { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
