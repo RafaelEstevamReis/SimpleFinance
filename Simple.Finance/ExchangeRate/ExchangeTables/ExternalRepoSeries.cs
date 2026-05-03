43df989d62aaebb1a2a34798a4b99888c8133a67
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-05-03 18:31:11 -0300

    Add ExternalTemporal Series

---
+++ Simple.Finance/ExchangeRate/ExchangeTables/ExternalRepoSeries.cs
@@ -0,0 +1,17 @@
+﻿namespace Simple.Finance.ExchangeRate.ExchangeTables;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ExternalRepoSeries
+{
+}
+public class ExternalDataModel
+{
+    public string QuoteCurrency { get; set; } = string.Empty;
+    public string BaseCurrency { get; set; } = string.Empty;
+    public DateTime FileGenerationDate { get; set; }
+
+    public Dictionary<int, Dictionary<string, decimal>> Values { get; set; } = [];
+}
